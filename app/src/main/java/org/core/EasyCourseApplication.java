4ec2cb3fd43b26b3572598060f256203aa28733d
timonin01 <atimonin2006@gmail.com>
2025-08-27 15:43:48 +0300

    first commit

---
+++ app/src/main/java/org/core/EasyCourseApplication.java
@@ -0,0 +1,13 @@
+package org.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyCourseApplication.class, args);
+    }
+
+}
