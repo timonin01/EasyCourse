f5ec912f7437b4ead1aa024112f211d57cf2421e
timonin01 <atimonin2006@gmail.com>
2025-08-29 14:51:54 +0300

    BE-21: Create LessonController

---
+++ app/src/main/java/org/core/rest/LessonController.java
@@ -0,0 +1,45 @@
+package org.core.rest;
+
+import jakarta.validation.Valid;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import org.core.dto.lesson.CreateLessonDTO;
+import org.core.dto.lesson.LessonResponseDTO;
+import org.core.dto.lesson.UpdateLessonDTO;
+import org.core.service.LessonService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/lessons")
+@RequiredArgsConstructor(access = AccessLevel.PACKAGE)
+public class LessonController {
+
+    private final LessonService lessonService;
+
+    @GetMapping("/{lessonId}")
+    public LessonResponseDTO getLessonByLessonID(@PathVariable Long lessonId) {
+        return lessonService.getLessonByLessonID(lessonId);
+    }
+
+    @GetMapping("/{modelId}")
+    public List<LessonResponseDTO> getModelLessonsByModelId(@PathVariable Long modelId) {
+        return lessonService.getModelLessonsByModelId(modelId);
+    }
+
+    @PostMapping
+    public LessonResponseDTO createLesson(@Valid @RequestBody CreateLessonDTO createDTO) {
+        return lessonService.createLesson(createDTO);
+    }
+
+    @PutMapping("/update")
+    public LessonResponseDTO updateLesson(@Valid @RequestBody UpdateLessonDTO updateDTO) {
+        return lessonService.updateLesson(updateDTO);
+    }
+
+    @DeleteMapping("/delete/{lessonId}")
+    public void deleteLesson(@PathVariable Long lessonId) {
+        lessonService.deleteLesson(lessonId);
+    }
+}
