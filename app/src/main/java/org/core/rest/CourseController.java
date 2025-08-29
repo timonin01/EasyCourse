c06fb4e31f0fcc334a1fd5fd60856743b800a839
timonin01 <atimonin2006@gmail.com>
2025-08-29 14:39:47 +0300

    BE-20: Create CourseController

---
+++ app/src/main/java/org/core/rest/CourseController.java
@@ -0,0 +1,46 @@
+package org.core.rest;
+
+import jakarta.validation.Valid;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import org.core.dto.course.CourseResponseDTO;
+import org.core.dto.course.CreateCourseDTO;
+import org.core.dto.course.UpdateCourseDTO;
+import org.core.service.CourseService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/courses")
+@RequiredArgsConstructor(access = AccessLevel.PACKAGE)
+public class CourseController {
+
+    private final CourseService courseService;
+
+    @GetMapping("/{courseId}")
+    public CourseResponseDTO getCourseByCourseId(@PathVariable Long courseId) {
+        return courseService.getCourseByCourseId(courseId);
+    }
+
+    @GetMapping("/{userId}")
+    public List<CourseResponseDTO> getUserCoursesByUserId(@PathVariable Long userId) {
+        return courseService.getUserCoursesByUserId(userId);
+    }
+
+    @PostMapping
+    public CourseResponseDTO createCourse(@Valid  @RequestBody CreateCourseDTO createDTO) {
+        return courseService.createCourse(createDTO);
+    }
+
+    @PutMapping("/update")
+    public CourseResponseDTO updateCourse(@Valid @RequestBody UpdateCourseDTO updateDTO) {
+        return courseService.updateCourse(updateDTO);
+    }
+
+    @DeleteMapping("/delete/{courseId}")
+    public void deleteCourse(@PathVariable Long courseId) {
+        courseService.deleteCourse(courseId);
+    }
+}
