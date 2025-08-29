26a8a30802315f305e1fc983c127f3ea2096d676
timonin01 <atimonin2006@gmail.com>
2025-08-29 14:52:04 +0300

    BE-21: Create StepController

---
+++ app/src/main/java/org/core/rest/StepController.java
@@ -0,0 +1,44 @@
+package org.core.rest;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import org.core.dto.step.CreateStepDTO;
+import org.core.dto.step.StepResponseDTO;
+import org.core.dto.step.UpdateStepDTO;
+import org.core.service.StepService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/steps")
+@RequiredArgsConstructor(access = AccessLevel.PACKAGE)
+public class StepController {
+
+    private final StepService stepService;
+
+    @GetMapping("/{stepId}")
+    public StepResponseDTO getStepById(@PathVariable Long stepId) {
+        return stepService.getStepById(stepId);
+    }
+
+    @GetMapping("/{lessonId}")
+    public List<StepResponseDTO> getLessonStepsByLessonId(@PathVariable Long lessonId) {
+        return stepService.getLessonStepsByLessonId(lessonId);
+    }
+
+    @PostMapping
+    public StepResponseDTO createStep(@RequestBody CreateStepDTO createStepDTO) {
+        return stepService.createStep(createStepDTO);
+    }
+
+    @PutMapping("/update")
+    public StepResponseDTO updateStep(@RequestBody UpdateStepDTO updateDto) {
+        return stepService.updateStep(updateDto);
+    }
+
+    @DeleteMapping("/delete/{stepId}")
+    public void deleteStep(@PathVariable Long stepId) {
+        stepService.deleteStep(stepId);
+    }
+}
