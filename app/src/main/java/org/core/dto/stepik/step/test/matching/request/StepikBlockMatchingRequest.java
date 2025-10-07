a6157a0d9cb3e05632cd5800a2de2a533a38e737
timonin01 <atimonin2006@gmail.com>
2025-10-07 19:51:23 +0300

    BE feat: create DTO for matching test step - create StepikBlockMatching request and response - create StepikSourceMatching request and response - create StepikPairMatching request and response

---
+++ app/src/main/java/org/core/dto/stepik/step/test/matching/request/StepikBlockMatchingRequest.java
@@ -0,0 +1,24 @@
+package org.core.dto.stepik.step.test.matching.request;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.core.dto.stepik.step.StepikBlockRequest;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class StepikBlockMatchingRequest implements StepikBlockRequest {
+
+    private String text;
+    private Object video = null;
+    private Object options = null;
+
+    private StepikMatchingSourceRequest source;
+
+}
+
---
+++ app/src/main/java/org/core/dto/stepik/step/test/matching/response/StepikMatchingPairResponse.java
@@ -0,0 +1,18 @@
+package org.core.dto.stepik.step.test.matching.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class StepikMatchingPairResponse {
+
+    private String first;
+    private String second;
+
+}
+
