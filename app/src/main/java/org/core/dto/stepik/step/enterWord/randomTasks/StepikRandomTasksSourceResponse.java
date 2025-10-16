b2b3d0218388de093b6039571975b6e8e3027190
timonin01 <atimonin2006@gmail.com>
2025-10-16 13:44:21 +0300

    BE add response DTOs for fill-blanks block

---
+++ app/src/main/java/org/core/dto/stepik/step/enterWord/randomTasks/StepikRandomTasksSourceResponse.java
@@ -0,0 +1,26 @@
+package org.core.dto.stepik.step.enterWord.randomTasks;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class StepikRandomTasksSourceResponse {
+
+    private String task;
+    private String solve;
+    @JsonProperty("max_error")
+    private String maxError;
+    private List<Object> ranges;
+    private Integer combinations;
+
+}
