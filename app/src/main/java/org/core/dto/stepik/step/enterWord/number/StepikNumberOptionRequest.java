af8b0d362b4caa8eacddbdc698ef2ba4412f811b
timonin01 <atimonin2006@gmail.com>
2025-10-16 13:17:03 +0300

    BE add request DTOs for fill-blanks block

---
+++ app/src/main/java/org/core/dto/stepik/step/enterWord/number/StepikNumberOptionRequest.java
@@ -0,0 +1,19 @@
+package org.core.dto.stepik.step.enterWord.number;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class StepikNumberOptionRequest {
+
+    private String answer;
+    private String max_error;
+
+}
